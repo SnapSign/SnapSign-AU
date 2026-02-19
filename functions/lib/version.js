b61165b2797363d596b0d2a60971ddfbae31eed8
Maxim Vasilkov <maxim.vasilkov@gmail.com>
2026-02-19 16:33:15 +0930

    feat: update application version and enhance use case pages for better clarity and navigation

--- functions/lib/version.js
+++ functions/lib/version.js
@@ -2,5 +2,5 @@
 // See package.json scripts.
 
 module.exports = {
-  APP_VERSION: "0.1.0+5e67bc0-2026-02-19T02:34:29.962Z",
+  APP_VERSION: "0.1.0+4470636-2026-02-19T06:56:55.575Z",
 };
--- snapsign.com.au/src/lib/version.js
+++ snapsign.com.au/src/lib/version.js
@@ -1,4 +1,4 @@
 // Auto-generated at build time.
 // See package.json "build" script.
 
-export const APP_VERSION = "0.1.0+5e67bc0-2026-02-19T02:34:13.059Z";
+export const APP_VERSION = "0.1.0+4470636-2026-02-19T06:55:40.315Z";
